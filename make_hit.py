79bf407201cfd04fd63e2edd3f3f4ddb31da8879
Sweyn Venderbush <sweyn3@yahoo.com>
2015-11-04 21:18:03 -0500

    Add secondary HIT

--- make_hit.py
+++ make_hit.py
@@ -30,7 +30,7 @@ def create_hit(url=None, title=None, description=None, keywords=None, reward_amo
     description = description or "This HIT must be completed within 15 minutes of it being posted. It will take less than 2 minutes. Date: %s" %(time)
     keywords = keywords or ["easy", "survey", "study", "bonus", "image", "images", "compare", "comparisons", "collection", "data", "research", "listings", "simple", "photo", "answer", "opinion", "question"]
     frame_height = 800
-    reward_amount = reward_amount or .25
+    reward_amount = reward_amount or .35
     max_assignments = max_assignments or 30
 
     duration_in_minutes = duration_in_minutes or 10
@@ -83,9 +83,11 @@ def create_queue(user_id, post_id):
 def make_hit_from_post(user_id, post_id):
     res = create_queue(user_id, post_id)
     queue_id = res['id']
-    qual = Requirement(QUAL, 'GreaterThan', 0)
-    response = create_hit(url="https://squadtest.herokuapp.com/?queueId=%s" % (queue_id), qualification_list = [qual])
-    hit_id = response[0].HITId
+    qual1 = Requirement(QUAL, 'GreaterThan', 0)
+    qual2 = Requirement(QUAL, 'DoesNotExist')
+    response1 = create_hit(url="https://squadtest.herokuapp.com/?queueId=%s" % (queue_id), reward_amount=.50, qualification_list = [qual1])
+    hit_id = response1[0].HITId
     worker_ids = [x.SubjectId for x in connection.get_all_qualifications_for_qual_type(QUAL)]
     send_workers_message(worker_ids, "[URGENT: 15 minutes to complete] A new Market Intelligence HIT has been posted", "A new HIT has been posted by Market Intelligence. It has HIT_ID %s. All our HITs can be found at our requester page (http://bit.ly/1Gmbre0) or by searching for Market Intelligence You are qualified to do the HIT. You have 15 minutes to complete the HIT." % (hit_id))
+    create_hit(url="https://squadtest.herokuapp.com/?queueId=%s" % (queue_id), reward_amount=.35, qualification_list = [qual2])
     
