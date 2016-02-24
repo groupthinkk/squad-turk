9d7e30fb386172f255af46e7db4e8039dfbeba9c
Sweyn Venderbush <sweyn3@yahoo.com>
2016-02-24 18:21:36 -0500

    Add twilio notify

--- twilio_notify.py
+++ twilio_notify.py
@@ -4,6 +4,7 @@ from config import Config
 import requests
 from random import shuffle
 from datetime import datetime, timedelta
+import sys
 
 account_sid = "AC92676a683900b40e7ba19d1b9a78a5ef"
 auth_token = "4de6b64136ddfcf839562af528f9304e"
@@ -31,6 +32,15 @@ def create_queue(user_id, post_id):
         print "POST Error ", res.text, data
     return res.json()
 
+def send_text(mes):
+    user_data = list(db['users'].find(None, {'phone_number': 1} ))
+    for i in xrange(len(user_data)):
+        phone_number = user_data[i]['phone_number']
+        try:
+            client.messages.create(to=phone_number, from_="+19292947687", body=mes)
+        except Exception, e:
+            print phone_number, e
+
 def send_texts_from_post(user_id, post_id):
     user_data = list(db['users'].find(None, {'phone_number': 1} ))
     all_queues = create_queue(user_id, post_id)
@@ -45,14 +55,15 @@ def send_texts_from_post(user_id, post_id):
 
 def send_texts_from_post_noqueue():
     now = datetime.now()
-    user_data = list(db['users'].find(None, {'phone_number': 1} ))
+    user_data = list(db['users'].find(None, {'phone_number': 1, 'last_queue': 1} ))
     for i in xrange(len(user_data)):
         phone_number = user_data[i]['phone_number']
         if now - user_data[i]['last_queue'] > timedelta(days=2):
         #queue_id = all_queues[i % len(all_queues)]['id']
         #queue_id = all_queues['id']
             try:
-                client.messages.create(to=phone_number, from_="+19292947687", body="Squad: You're falling in the leaderboard! Play now to stay ahead! http://goo.gl/Mt1Wa0")
+                client.messages.create(to=phone_number, from_="+19292947687", body="Squad: You're falling in the leaderboard over the last 2 days! Play now to stay ahead! http://goo.gl/Mt1Wa0")   
+                db['users'].update({'phone_number': phone_number}, {"$set": {"last_queue": datetime.now() - timedelta(hours = 1)}})
             except Exception, e:
                 print phone_number, e
     # queue_type_list = ['food', 'fashion', 'sports']
@@ -64,4 +75,7 @@ def send_texts_from_post_noqueue():
     # )
 
 if __name__ == '__main__':
-    send_texts_from_post_noqueue()
+    if len(sys.argv) > 1 and sys.argv[1] == "0":
+        send_text("Squad: Good news! You now have unlimited queues! Log in any time to do a queue based on a specific Instagram account of your choice. Which will you master? http://goo.gl/Mt1Wa0")
+    else:
+        send_texts_from_post_noqueue()
